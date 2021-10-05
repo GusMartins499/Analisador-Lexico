a195e241e4a1563c473c033875ba7ff79f416318
Gustavo Martins <atendimento9@cubosis.com.br>
2021-10-05 01:03:54 +0100

    readFile

--- src/AppSystem.java
+++ src/AppSystem.java
@@ -1,5 +1,9 @@
 
 public class AppSystem {
+	
+	public static void initialize() {
+		FileMananger.open_file("archive.txt");
+	}
 	/*
 	 * Responsavel por chamar todos os metodos
 	 * 
--- src/FileMananger.java
+++ src/FileMananger.java
@@ -1,8 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class FileMananger {
+	public static void open_file(String name) {
+		try {
+			FileReader file = new FileReader(name);
+			BufferedReader read_file = new BufferedReader(file);
+
+			String linha = read_file.readLine();
+
+			while (linha != null) {
+				System.out.printf("%s\n", linha);
+
+				linha = read_file.readLine();
+			}
+			file.close();
+		} catch (Exception e) {
+			System.out.println("error " + e);
+			// TODO: handle exception
+		}
+	}
 	/*
-	 * Abrir arquivo
-	 * Fechar arquivo
-	 * Ler arquivo => retorno é um array de caracteres
+	 * Abrir arquivo Fechar arquivo Ler arquivo => retorno é um array de caracteres
 	 */
 }
