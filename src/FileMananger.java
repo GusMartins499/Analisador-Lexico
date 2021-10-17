ba66fa1d8cf5cd20dabe655059c721de54e33fc3
Gustavo Martins <atendimento9@cubosis.com.br>
2021-10-17 20:55:27 +0100

    retorno unico vetor de todas as linhas do arquivo

--- src/FileMananger.java
+++ src/FileMananger.java
@@ -2,23 +2,36 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class FileMananger {
-	public static void open_file(String name) {
+	public static char[] open_file(String name) {
 		try {
 			FileReader file = new FileReader(name);
 			BufferedReader read_file = new BufferedReader(file);
 
 			String linha = read_file.readLine();
-
+			String linhaConcat = linha;
+			
+			char [] vetorTodasLinhas = null;
+			
 			while (linha != null) {
-				System.out.printf("%s\n", linha);
-
 				linha = read_file.readLine();
+				if (linha != null) {
+					linhaConcat += linha;
+				}
 			}
+			
+			vetorTodasLinhas = linhaConcat.toCharArray();
+			
 			file.close();
+			
+			for (int i = 0; i < vetorTodasLinhas.length; i++) {
+				System.out.println(vetorTodasLinhas[i]);
+			}
+			return vetorTodasLinhas;
+			
 		} catch (Exception e) {
 			System.out.println("error " + e);
-			// TODO: handle exception
 		}
+		return null;
 	}
 	/*
 	 * Abrir arquivo Fechar arquivo Ler arquivo => retorno é um array de caracteres
